97fcfaba99249db9e30cac8febfceba72e44ee9d
moreshwar.madaye@learningmate.com <moreshwar.madaye@learningmate.com>
2015-08-13 17:04:43 +0530

    Moreshwar QST-123 Commit

---
+++ sql/user_session.sql
@@ -0,0 +1,31 @@
+/*
+SQLyog Community v11.52 (32 bit)
+MySQL - 5.0.77 : Database - phn20_nightly
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `UserSession` */
+
+DROP TABLE IF EXISTS `UserSession`;
+
+CREATE TABLE `UserSession` (
+  `SessionId` varchar(255) NOT NULL,
+  `SessionData` longtext,
+  `SessionTime` int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`SessionId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `UserSession` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
