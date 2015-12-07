4bba57275b64ae4bbea8f4675bea6d415c3163b5
moreshwar.madaye@learningmate.com <moreshwar.madaye@learningmate.com>
2015-12-07 19:50:56 +0530

    This is Moreshwar. QST_1279 Changes
    Added Changes 1

--- sql/user_session.sql
+++ sql/user_session.sql
@@ -29,3 +29,8 @@ CREATE TABLE `UserSession` (
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+
+// Chnages added for qst_1278 Start :: 1
+
+// Chnages added for qst_1278 End :: 1
